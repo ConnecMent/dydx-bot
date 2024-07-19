38e480878388d24d77a1774bd76826ca85987c0e
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2024-07-20 03:16:35 +0330

    write service function :pill:

---
+++ strategy-service.ts
@@ -0,0 +1,5 @@
+import { strategyType, type Candle } from "./types/common";
+
+function strategyService(candle: Candle): strategyType {
+  return strategyType.null;
+}
--- types/common.ts
+++ types/common.ts
@@ -6,3 +6,5 @@ export type Candle = any;
 
 export type Pair = string;
 export type PositionType = "long" | "short" | null;
+
+export enum strategyType {long, short, null, no_trade};
