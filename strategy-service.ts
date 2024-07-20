67df813c6d5185030ea9b68fb641f8f035c16762
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2024-07-20 20:06:51 +0330

    exported function :rabbit2:

--- strategy-service.ts
+++ strategy-service.ts
@@ -1,6 +1,6 @@
 import { type PositionType, type Pair, type Candle } from "./types/common";
 
-function strategyService(record: { [key: Pair]: Array<Candle> }): {
+export function strategyService(record: { [key: Pair]: Array<Candle> }): {
   [key: Pair]: PositionType;
 } {
   return { pair1: "long" };
