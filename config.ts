5eee2cba682ce8052289754d015c722e8f35dfe1
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2024-08-08 19:17:11 +0330

    use ES6 modules import in config :100:

--- config.ts
+++ config.ts
@@ -1,5 +1,8 @@
 // import env file with 'dotenv'
-require('dotenv').config({ path: '.env' });
+// require('dotenv').config({ path: '.env' });
+
+import dotenv from 'dotenv';
+dotenv.config({ path: '.env' });
 
 const config = {
   interval: Number(process.env.INTERVAL),
