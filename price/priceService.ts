8a292a02ee62f85d8b0d1c9d57e7df370a9e0d44
ramin <ramin.lgh.82@gmail.com>
2024-08-08 16:32:17 +0330

    edit function

--- price/priceService.ts
+++ price/priceService.ts
@@ -1,37 +1,20 @@
-import {baseUrl} from "./url.js";
+import { baseUrl } from "./url.js";
 
 type Pair = string;
 type TimeFrame = string;
 
+async function fetchData(pair: Pair, timeFrame: TimeFrame): Promise<string> {
+  const response = await fetch(
+    `${baseUrl}/candles/perpetualMarkets/${pair}?resolution=${timeFrame}`
+  );
 
-async function fetchData(pair:Pair,timeFrame:TimeFrame): Promise<string> {
-    
-    try {
-        
-        const response = await fetch(`${baseUrl}/candles/perpetualMarkets/${pair}?resolution=${timeFrame}`);
+  if (!response.ok) {
+    throw new Error(
+      "Network response was not ok or maybe input not valid pair and timeFrame"
+    );
+  }
 
-        if (!response.ok) {
-            throw new Error('Network response was not ok or maybe input not valid pair and timeFrame');
-        }
+  const data = await response.json();
 
-        const data = await response.json();
-
-        return data;
-
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        throw error;
-    }
-
+  return data;
 }
-
-
-
-
-
-
-
-
-
-
-    
