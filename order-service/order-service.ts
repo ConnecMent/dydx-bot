a3cf218e2118abe81fdaf3b35377564408ce21b9
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2024-09-03 17:22:32 +0330

    refactor(order service): init client in func body

--- order-service/order-service.ts
+++ order-service/order-service.ts
@@ -10,216 +10,191 @@ import {
 } from '@dydxprotocol/v4-client-js';
 
 // order factory function
-const createOrderService = (mnemonic: string, network: Network) => ({
-  placeLimitOrder: (
-    pair: Pair,
-    side: Side,
-    price: number,
-    size: number,
-    config: OrderConfig,
-  ) => {
-    return new Promise<Tx>(async (resolve) => {
-      const wallet = await LocalWallet.fromMnemonic(mnemonic, BECH32_PREFIX);
-      const subaccount = new SubaccountClient(wallet, 0);
+const createOrderService = async (mnemonic: string, network: Network) => {
+  const wallet = await LocalWallet.fromMnemonic(mnemonic, BECH32_PREFIX);
+  const subaccount = new SubaccountClient(wallet, 0);
+  const client = await CompositeClient.connect(network);
 
-      const client = await CompositeClient.connect(network);
+  const indexer_client = new IndexerClient(network.indexerConfig);
 
-      const tx = await client.placeOrder(
-        subaccount,
-        pair,
-        OrderType.LIMIT,
-        side,
-        price,
-        size,
-        config.clientId,
-        config.timeInForce,
-        config.goodTilTimeInSeconds,
-        config.execution,
-        config.postOnly,
-        false,
-        config.triggerPrice,
-      );
+  return {
+    placeLimitOrder: (
+      pair: Pair,
+      side: Side,
+      price: number,
+      size: number,
+      config: OrderConfig,
+    ) => {
+      return new Promise<Tx>((resolve) => {
+        resolve(
+          client.placeOrder(
+            subaccount,
+            pair,
+            OrderType.LIMIT,
+            side,
+            price,
+            size,
+            config.clientId,
+            config.timeInForce,
+            config.goodTilTimeInSeconds,
+            config.execution,
+            config.postOnly,
+            false,
+            config.triggerPrice,
+          ),
+        );
+      });
+    },
+    placeMarketOrder: (
+      pair: Pair,
+      side: Side,
+      price: number,
+      size: number,
+      config: OrderConfig,
+    ) => {
+      return new Promise<Tx>((resolve) => {
+        resolve(
+          client.placeOrder(
+            subaccount,
+            pair,
+            OrderType.MARKET,
+            side,
+            price,
+            size,
+            config.clientId,
+            config.timeInForce,
+            config.goodTilTimeInSeconds,
+            config.execution,
+            config.postOnly,
+            false,
+            config.triggerPrice,
+          ),
+        );
+      });
+    },
+    placeLimitTakeProfitOrder: (
+      pair: Pair,
+      side: Side,
+      price: number,
+      size: number,
+      config: OrderConfig,
+    ) => {
+      return new Promise<Tx>((resolve) => {
+        resolve(
+          client.placeOrder(
+            subaccount,
+            pair,
+            OrderType.TAKE_PROFIT_LIMIT,
+            side,
+            price,
+            size,
+            config.clientId,
+            config.timeInForce,
+            config.goodTilTimeInSeconds,
+            config.execution,
+            config.postOnly,
+            true,
+            config.triggerPrice,
+          ),
+        );
+      });
+    },
+    placeMarketTakeProfitOrder: (
+      pair: Pair,
+      side: Side,
+      price: number,
+      size: number,
+      config: OrderConfig,
+    ) => {
+      return new Promise<Tx>((resolve) => {
+        resolve(
+          client.placeOrder(
+            subaccount,
+            pair,
+            OrderType.TAKE_PROFIT_MARKET,
+            side,
+            price,
+            size,
+            config.clientId,
+            config.timeInForce,
+            config.goodTilTimeInSeconds,
+            config.execution,
+            config.postOnly,
+            true,
+            config.triggerPrice,
+          ),
+        );
+      });
+    },
+    placeLimitStopLossOrder: (
+      pair: Pair,
+      side: Side,
+      price: number,
+      size: number,
+      config: OrderConfig,
+    ) => {
+      return new Promise<Tx>((resolve) => {
+        resolve(
+          client.placeOrder(
+            subaccount,
+            pair,
+            OrderType.STOP_LIMIT,
+            side,
+            price,
+            size,
+            config.clientId,
+            config.timeInForce,
+            config.goodTilTimeInSeconds,
+            config.execution,
+            config.postOnly,
+            true,
+            config.triggerPrice,
+          ),
+        );
+      });
+    },
+    placeMarketStopLossOrder: (
+      pair: Pair,
+      side: Side,
+      price: number,
+      size: number,
+      config: OrderConfig,
+    ) => {
+      return new Promise<Tx>((resolve) => {
+        resolve(
+          client.placeOrder(
+            subaccount,
+            pair,
+            OrderType.STOP_MARKET,
+            side,
+            price,
+            size,
+            config.clientId,
+            config.timeInForce,
+            config.goodTilTimeInSeconds,
+            config.execution,
+            config.postOnly,
+            true,
+            config.triggerPrice,
+          ),
+        );
+      });
+    },
+    listPositions: () => {
+      return new Promise<Position[]>(async (resolve) => {
+        const subAccRes = await indexer_client.account.getSubaccounts(
+          wallet.address || '',
+        );
 
-      resolve(tx);
-    });
-  },
-  placeMarketOrder: (
-    pair: Pair,
-    side: Side,
-    price: number,
-    size: number,
-    config: OrderConfig,
-  ) => {
-    return new Promise<Tx>(async (resolve) => {
-      const wallet = await LocalWallet.fromMnemonic(mnemonic, BECH32_PREFIX);
-      const subaccount = new SubaccountClient(wallet, 0);
+        const response =
+          await indexer_client.account.getSubaccountAssetPositions(
+            wallet.address || '',
+            subAccRes.subaccounts[0],
+          );
+        const positions = response.positions;
 
-      const client = await CompositeClient.connect(network);
-
-      const tx = await client.placeOrder(
-        subaccount,
-        pair,
-        OrderType.MARKET,
-        side,
-        price,
-        size,
-        config.clientId,
-        config.timeInForce,
-        config.goodTilTimeInSeconds,
-        config.execution,
-        config.postOnly,
-        false,
-        config.triggerPrice,
-      );
-
-      resolve(tx);
-    });
-  },
-  placeLimitTakeProfitOrder: (
-    pair: Pair,
-    side: Side,
-    price: number,
-    size: number,
-    config: OrderConfig,
-  ) => {
-    return new Promise<Tx>(async (resolve) => {
-      const wallet = await LocalWallet.fromMnemonic(mnemonic, BECH32_PREFIX);
-      const subaccount = new SubaccountClient(wallet, 0);
-
-      const client = await CompositeClient.connect(network);
-
-      const tx = await client.placeOrder(
-        subaccount,
-        pair,
-        OrderType.TAKE_PROFIT_LIMIT,
-        side,
-        price,
-        size,
-        config.clientId,
-        config.timeInForce,
-        config.goodTilTimeInSeconds,
-        config.execution,
-        config.postOnly,
-        true,
-        config.triggerPrice,
-      );
-
-      resolve(tx);
-    });
-  },
-  placeMarketTakeProfitOrder: (
-    pair: Pair,
-    side: Side,
-    price: number,
-    size: number,
-    config: OrderConfig,
-  ) => {
-    return new Promise<Tx>(async (resolve) => {
-      const wallet = await LocalWallet.fromMnemonic(mnemonic, BECH32_PREFIX);
-      const subaccount = new SubaccountClient(wallet, 0);
-
-      const client = await CompositeClient.connect(network);
-
-      const tx = await client.placeOrder(
-        subaccount,
-        pair,
-        OrderType.TAKE_PROFIT_MARKET,
-        side,
-        price,
-        size,
-        config.clientId,
-        config.timeInForce,
-        config.goodTilTimeInSeconds,
-        config.execution,
-        config.postOnly,
-        true,
-        config.triggerPrice,
-      );
-
-      resolve(tx);
-    });
-  },
-  placeLimitStopLossOrder: (
-    pair: Pair,
-    side: Side,
-    price: number,
-    size: number,
-    config: OrderConfig,
-  ) => {
-    return new Promise<Tx>(async (resolve) => {
-      const wallet = await LocalWallet.fromMnemonic(mnemonic, BECH32_PREFIX);
-      const subaccount = new SubaccountClient(wallet, 0);
-
-      const client = await CompositeClient.connect(network);
-
-      const tx = await client.placeOrder(
-        subaccount,
-        pair,
-        OrderType.STOP_LIMIT,
-        side,
-        price,
-        size,
-        config.clientId,
-        config.timeInForce,
-        config.goodTilTimeInSeconds,
-        config.execution,
-        config.postOnly,
-        true,
-        config.triggerPrice,
-      );
-
-      resolve(tx);
-    });
-  },
-  placeMarketStopLossOrder: (
-    pair: Pair,
-    side: Side,
-    price: number,
-    size: number,
-    config: OrderConfig,
-  ) => {
-    return new Promise<Tx>(async (resolve) => {
-      const wallet = await LocalWallet.fromMnemonic(mnemonic, BECH32_PREFIX);
-      const subaccount = new SubaccountClient(wallet, 0);
-
-      const client = await CompositeClient.connect(network);
-
-      const tx = await client.placeOrder(
-        subaccount,
-        pair,
-        OrderType.STOP_MARKET,
-        side,
-        price,
-        size,
-        config.clientId,
-        config.timeInForce,
-        config.goodTilTimeInSeconds,
-        config.execution,
-        config.postOnly,
-        true,
-        config.triggerPrice,
-      );
-
-      resolve(tx);
-    });
-  },
-  listPositions: () => {
-    return new Promise<Position[]>(async (resolve) => {
-      const wallet = await LocalWallet.fromMnemonic(mnemonic, BECH32_PREFIX);
-
-      const client = new IndexerClient(network.indexerConfig);
-
-      const subAccRes = await client.account.getSubaccounts(
-        wallet.address || '',
-      );
-
-      const response = await client.account.getSubaccountAssetPositions(
-        wallet.address || '',
-        subAccRes.subaccounts[0],
-      );
-      const positions = response.positions;
-
-      resolve(positions);
-    });
-  },
-});
+        resolve(positions);
+      });
+    },
+  };
+};
