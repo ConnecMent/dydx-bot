a9d6a80dc5c68431adeb70661e7d1762c8a69bd8
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2024-10-09 17:58:32 +0330

    chore: replaced unknown with any :ski:

--- types/strategy.ts
+++ types/strategy.ts
@@ -14,21 +14,21 @@ export interface Strategy {
 }
 
 export const strategySchema: Readonly<{
-  platform: (value: any) => boolean;
-  pair: (value: any) => boolean;
-  timeframe: (value: any) => boolean;
-  planPlugin: (value: any) => boolean;
-  executePlugin: (value: any) => boolean;
-  managePlugin: (value: any) => boolean;
+  platform: (value: unknown) => boolean;
+  pair: (value: unknown) => boolean;
+  timeframe: (value: unknown) => boolean;
+  planPlugin: (value: unknown) => boolean;
+  executePlugin: (value: unknown) => boolean;
+  managePlugin: (value: unknown) => boolean;
 }> = {
-  platform: (value: any): boolean => value === 'dydx',
-  pair: (value: any): boolean => typeof value === 'string',
-  timeframe: (value: any): boolean =>
+  platform: (value: unknown): boolean => value === 'dydx',
+  pair: (value: unknown): boolean => typeof value === 'string',
+  timeframe: (value: unknown): boolean =>
     typeof value === 'string' &&
     ['1MIN', '5MINS', '15MINS', '30MINS', '1HOUR', '4HOURS', '1DAY'].includes(
       value,
     ),
-  planPlugin: (value: any): boolean => typeof value === 'string',
-  executePlugin: (value: any): boolean => typeof value === 'string',
-  managePlugin: (value: any): boolean => typeof value === 'string',
+  planPlugin: (value: unknown): boolean => typeof value === 'string',
+  executePlugin: (value: unknown): boolean => typeof value === 'string',
+  managePlugin: (value: unknown): boolean => typeof value === 'string',
 };
