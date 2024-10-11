e612853eb7db427dce6d0befc1fe2cf5f014bcfb
Mohammad Kermani <mkermani144@gmail.com>
2024-10-11 19:48:48 +0330

    feat: add log configs to config module

--- config.ts
+++ config.ts
@@ -5,6 +5,8 @@ dotenv.config({ path: ['.env.local', '.env'] });
 
 const config = {
   interval: Number(process.env.INTERVAL),
+  logFile: process.env.LOG_FILE!,
+  logFrequency: process.env.LOG_FREQUENCY!,
 };
 
 export default config;
