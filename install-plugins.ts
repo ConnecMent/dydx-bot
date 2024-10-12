b4bed5021c206a76198a0e422a7e91a5e8dd8dfc
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2024-10-12 21:37:35 +0330

    create installation core

--- install-plugins.ts
+++ install-plugins.ts
@@ -1,9 +1,35 @@
 import { Plugin } from './types/plugin.js';
+import download from 'download';
+import fs from 'fs';
+import os from 'os';
 
 export default async function installPlugins(
   pluginURLs: string[],
 ): Promise<Record<string, Plugin>> {
-  const plugins = {};
+  const plugins: Record<string, Plugin> = {};
+
+  const saveDir = os.homedir() + '/dydx-bot/plugins';
+
+  if (!fs.existsSync(saveDir)) {
+    fs.mkdirSync(saveDir, { recursive: true });
+  }
+
+  for (const url of pluginURLs) {
+    try {
+      await download(url, saveDir, { filename: 'temp' });
+
+      const fileContent = fs.readFileSync(`${saveDir}/temp`);
+
+      const plugin: Plugin = JSON.parse(fileContent.toString());
+      plugins[plugin.name] = plugin;
+
+      fs.writeFileSync(`${saveDir}/${plugin.name}.json`, fileContent);
+    } catch (e) {
+      console.error('Failed to download plugin:', e);
+    } finally {
+      fs.unlinkSync(`${saveDir}/temp`);
+    }
+  }
 
   return plugins;
 }
