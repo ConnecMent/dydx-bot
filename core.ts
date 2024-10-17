04e540370d58b503651cb7a7cbf08ae15b6eaf9b
Mohammad Kermani <mkermani144@gmail.com>
2024-10-17 15:14:24 +0330

    feat: add logs to core module

--- core.ts
+++ core.ts
@@ -3,13 +3,16 @@ import config from './config.js';
 
 import { installPlugins } from './install-plugins.js';
 import { loadStrategies } from './load-strategies.js';
+import logger from './log-service.js';
 import createOrderService from './order-service/order-service.js';
 import { fetchCandles } from './price/priceService.js';
 
-import { Order } from './types/common.js';
 import { ExecutePlugin, ManagePlugin, PlanPlugin } from './types/plugin.js';
 
+logger.info('Starting bot');
+
 const strategies = await loadStrategies();
+logger.info('Strategies loaded');
 const requiredPluginNames = [
   ...new Set(
     strategies.flatMap((strategy) => [
@@ -20,44 +23,45 @@ const requiredPluginNames = [
   ),
 ];
 
-/**
- * TODO: Step 2 - Install required plugins
- * https://github.com/ConnecMent/dydx-bot/issues/27
- */
+logger.debug('Installing or loading required plugins', requiredPluginNames);
 const plugins = await installPlugins(requiredPluginNames);
-
-const orders: Order[] = [];
+logger.info('Plugins installed or loaded');
 
 // Step 3: Run
-const orderService = await createOrderService('', Network.mainnet());
+logger.info('Order service created');
 
 const main = () => {
   strategies.forEach(async (strategy) => {
+    logger.info(`Running strategy with id ${strategy.id}`);
     const candles = await fetchCandles(strategy.pair, strategy.timeframe);
+    logger.info(
+      `Candles for ${strategy.pair} in timeframe ${strategy.timeframe} fetched`,
+    );
 
     const side = (plugins[strategy.planPlugin] as PlanPlugin).chooseSide(
       candles,
     );
+    logger.info(`Plan plugin ran over fetched candles`);
+    logger.info(`Plan plugin result: ${side}`);
 
     if (side) {
-      /**
-       * TODO: Call `execute` with bot context
-       * https://github.com/ConnecMent/dydx-bot/issues/4
-       */
-      (plugins[strategy.executePlugin] as ExecutePlugin).execute(
+      logger.debug(
+        'Running execute plugin because plan plugin result was not null',
+      );
+
+      await (plugins[strategy.executePlugin] as ExecutePlugin).execute(
         candles,
         side,
         strategy.pair,
         orderService,
       );
+      logger.info('Execute plugin ran based on plan');
     }
 
-    /**
-     * TODO: Call `manage` with bot context
-     * https://github.com/ConnecMent/dydx-bot/issues/4
-     */
-    (plugins[strategy.managePlugin] as ManagePlugin).manage(orders);
+    (plugins[strategy.managePlugin] as ManagePlugin).manage(orderService);
+    logger.info('Manage plugin ran');
   });
 };
 
+main();
 setInterval(main, config.interval);
